51a3bdf8da21dae1f1c83990e26667136eb01b63
Ilarbe <ollie.wyeth@gmail.com>
2019-11-04 10:39:38 -0500

    Functions -> Classes
    Changed functional components to class based components

--- src/posts/CardDetail.js
+++ src/posts/CardDetail.js
@@ -1,32 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import MthData from '../data/monthly.json'
 
-const curr = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2
-})
+class CardDetail extends Component {
+  render () {
+    const curr = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2
+    })
 
-var amountSum = MthData.reduce(function (sum, d) {
-  return sum + d.amount
-}, 0)
+    var amountSum = MthData.reduce(function (sum, d) {
+      return sum + d.amount
+    }, 0)
 
-function CardList () {
-  return (
-    <div className='col-6' key={key}>
-      <div className='card'>
-        <h5 className='card-header'>
-          {person.title}
-        </h5>
-        <div className='card-body'>
-          <h6 className='card-title'>Income</h6>
-          <p className='card-text'>{curr.format(person.amount)}</p>
-          <h6 className='card-title'>Contribution</h6>
-          <p className='card-text'>{(person.amount / amountSum * 100).toFixed(1) + '%'}</p>
+    const { person } = this.props
+
+    return (
+      <div className='col-6'>
+        <div className='card'>
+          <h5 className='card-header'>
+            {person.title}
+          </h5>
+          <div className='card-body'>
+            <h6 className='card-title'>Income</h6>
+            <p className='card-text'>{curr.format(person.amount)}</p>
+            <h6 className='card-title'>Contribution</h6>
+            <p className='card-text'>{(person.amount / amountSum * 100).toFixed(1) + '%'}</p>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default CardList
+export default CardDetail
--- src/posts/CardList.js
+++ src/posts/CardList.js
@@ -1,42 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import MthData from '../data/monthly.json'
 
-const curr = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2
-})
-
-var amountSum = MthData.reduce(function (sum, d) {
-  return sum + d.amount
-}, 0)
-
-const cardElement = (key, person) => {
-  return (
-    <div className='col-6' key={key}>
-      <div className='card'>
-        <h5 className='card-header'>
-          {person.title}
-        </h5>
-        <div className='card-body'>
-          <h6 className='card-title'>Income</h6>
-          <p className='card-text'>{curr.format(person.amount)}</p>
-          <h6 className='card-title'>Contribution</h6>
-          <p className='card-text'>{(person.amount / amountSum * 100).toFixed(1) + '%'}</p>
-        </div>
+import CardDetail from './CardDetail'
+class CardList extends Component {
+  render () {
+    return (
+      <div className='row no-gutters'>
+        {MthData.map((personDetail, index) => {
+          return <CardDetail person={personDetail} key={`post-list-key ${index}`} />
+        })}
       </div>
-    </div>
-  )
-}
-
-function CardList () {
-  return (
-    <div className='row no-gutters'>
-      {MthData.map((personDetail, index) => {
-        return cardElement(index, personDetail)
-      })}
-    </div>
-  )
+    )
+  }
 }
 
 export default CardList
