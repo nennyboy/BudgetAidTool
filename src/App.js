c5fff788f037b318878eab4d0e1106575a5431c6
Ilarbe <ollie.wyeth@gmail.com>
2019-11-08 10:54:41 -0500

    Mobile Adaptation
    - Remove blue border from buttons (v1.0)
    - Remove scroll bounce on mobile (v1.0)
    - Add padding between header and top of page

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import PersonList from './person/PersonList'
 import BudgetTool from './person/BudgetTool'
 import './bootstrap.min.css'
 import './App.css'
-import { Container, Tab, Tabs } from 'react-bootstrap'
+import { Container, Tab, Tabs, Row, Col } from 'react-bootstrap'
 
 function App () {
   return (
     <Container>
-      <h1 className='text-center'>Budget Tool</h1>
-      <hr />
-      <Tabs defaultActiveKey='budgetTool' id='uncontrolled-tab-example'>
-        <Tab eventKey='budgetTool' title='Budget Tool'>
-          <BudgetTool />
-        </Tab>
-        <Tab eventKey='personDetail' title='Person Detail'>
-          <div>
-            <p className='p-1'>Select an individual to see how their income is distributed across costs</p>
-          </div>
-          <PersonList />
-        </Tab>
+      <Row>
+        <Col>
+          <h1 className='text-center my-3 py-3'>Budget Tool</h1>
+          <hr />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Tabs defaultActiveKey='budgetTool' id='uncontrolled-tab-example'>
+            <Tab eventKey='budgetTool' title='Budget Tool'>
+              <BudgetTool />
+            </Tab>
+            <Tab eventKey='personDetail' title='Person Detail'>
+              <div>
+                <p className='p-1'>Select an individual to see how their income is distributed across costs</p>
+              </div>
+              <PersonList />
+            </Tab>
 
-      </Tabs>
+          </Tabs>
+        </Col>
+      </Row>
     </Container>
   )
 }
