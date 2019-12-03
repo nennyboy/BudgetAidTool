4115b1d29f26c1e8b686cd379e689d0900bf4430
nennyboy <ollie.wyeth@gmail.com>
2019-12-03 11:43:39 -0500

    Tooltip Commit v0.1c
    Tooltip Commit v0.1c

--- src/person/persondetail/persondiff/PersonDiffRow.js
+++ src/person/persondetail/persondiff/PersonDiffRow.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Tooltip, OverlayTrigger } from 'react'
 // import AccData from '../../../data/accounts.json'
 
 class PersonDiffRow extends Component {
