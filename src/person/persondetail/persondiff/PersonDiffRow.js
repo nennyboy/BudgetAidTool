5a57016751c467a0adf5e3140cad3fe3371f38e9
nennyboy <ollie.wyeth@gmail.com>
2019-12-03 13:12:14 -0500

    Update PersonDiffRow.js

--- src/person/persondetail/persondiff/PersonDiffRow.js
+++ src/person/persondetail/persondiff/PersonDiffRow.js
@@ -41,7 +41,7 @@ class PersonDiffRow extends Component {
 			  <td>
 					<OverlayTrigger
 						placement='left'
-						overlay={<Tooltip>{curr.format((accAmount(acct)*3))} | <small><i>*3</i></small></Tooltip>}
+						overlay={<Tooltip>{curr.format((accAmount(acct)*3))} | <small>*3</small></Tooltip>}
 					  >
 						<span>{curr.format(accAmount(acct))}</span>
 					  </OverlayTrigger>
